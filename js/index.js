85c6d1939488537a93336336e29804e123b09e5f
Oleksandr Rozhkov <alexshura@gmail.com>
2018-11-08 22:09:21 +0000

    pre final

---
+++ js/index.js
@@ -0,0 +1,32 @@
+/*const links = [['aboutKor','https://www.korculainfo.com/general/'],['trInf','https://www.korculainfo.com/travel/'],['mapsKor','https://www.korculainfo.com/maps/'],['histCult','https://www.korculainfo.com/culture/'],['activat','https://www.korculainfo.com/sport-korcula/']];
+
+const gotoLink = document.getElementByClass('button-friendlylinks');
+    gotoLink.addEventListener('click', function (go) {
+        if (button-friendlylinks) {alert('')}
+        else {location.href = 'http://www./'});
+*/
+    
+const gotoLink1 = document.getElementById('aboutKor');
+    gotoLink1.addEventListener('click', function (go) {
+        location.href = 'https://www.korculainfo.com/general/'});
+    
+const gotoLink2 = document.getElementById('trInf');
+    gotoLink2.addEventListener('click', function (go) {
+        location.href = 'https://www.korculainfo.com/travel/'});
+
+const gotoLink3 = document.getElementById('mapsKor');
+    gotoLink3.addEventListener('click', function (go) {
+        location.href = 'https://www.korculainfo.com/maps/'});
+
+const gotoLink4 = document.getElementById('histCult');
+    gotoLink4.addEventListener('click', function (go) {
+        location.href = 'https://www.korculainfo.com/culture/'});
+
+const gotoLink5 = document.getElementById('activat');
+    gotoLink5.addEventListener('click', function (go) {
+        location.href = 'https://www.korculainfo.com/sport-korcula/'});
+    
+    
+    
+    
+    
