445ccc297358a1a6839899013a409c5bf2a8fc3a
Oleksandr Rozhkov <alexshura@gmail.com>
2018-11-09 12:07:14 +0000

    add smooth

--- js/index.js
+++ js/index.js
@@ -1,11 +1,3 @@
-/*const links = [['aboutKor','https://www.korculainfo.com/general/'],['trInf','https://www.korculainfo.com/travel/'],['mapsKor','https://www.korculainfo.com/maps/'],['histCult','https://www.korculainfo.com/culture/'],['activat','https://www.korculainfo.com/sport-korcula/']];
-
-const gotoLink = document.getElementByClass('button-friendlylinks');
-    gotoLink.addEventListener('click', function (go) {
-        if (button-friendlylinks) {alert('')}
-        else {location.href = 'http://www./'});
-*/
-    
 const gotoLink1 = document.getElementById('aboutKor');
     gotoLink1.addEventListener('click', function (go) {
         location.href = 'https://www.korculainfo.com/general/'});
